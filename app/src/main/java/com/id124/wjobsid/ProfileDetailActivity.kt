7ec96fdcc3000927324299dba3a47979192dda31
ndraa124 <indradavidpesik@gmail.com>
2020-12-05 15:07:00 +0800

    add activity

--- app/src/main/java/com/id124/wjobsid/ProfileDetailActivity.kt
+++ app/src/main/java/com/id124/wjobsid/ProfileDetailActivity.kt
@@ -1,11 +1,29 @@
 package com.id124.wjobsid
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_profile_detail.*
 
-class ProfileDetailActivity : AppCompatActivity() {
+class ProfileDetailActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile_detail)
+
+        tab_layout.setupWithViewPager(view_pager)
+        val adapter = PerformancePagerAdapter(supportFragmentManager)
+
+        adapter.addFrag(PortfolioFragment(), "Portfolio")
+        adapter.addFrag(ExperienceFragment(), "Experience")
+        view_pager.adapter = adapter
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btn_hire -> {
+                Log.d("msg", "Hiring Process")
+            }
+        }
     }
 }
